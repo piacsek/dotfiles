b33fbe9162755ee10d8bfd9036c606862deab584
piacsek <felipe@piacsek.me>
2024-02-08 14:10:09 -0800

    Removing nvim because I'll start over

--- nvim/chadrc.lua
+++
@@ -1,7 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = 'github_dark' }
-M.plugins = "custom.plugins"
-
-return M
--- nvim/init.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], {desc = 'Escape from terminal mode'})
