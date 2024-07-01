2a7b2f58a4e1652ed647129724d281063905cbae
piacsek <felipe@piacsek.me>
2024-06-30 22:56:28 -0700

    nvim: adds a bunch of plugins

--- nvim/lua/piacsek/init.lua
+++ nvim/lua/piacsek/init.lua
@@ -1,3 +1,13 @@
 require("piacsek.set")
 require("piacsek.remap")
 require("piacsek.lazy_init")
+
+
+-- Which file should I put this?
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
