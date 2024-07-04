5478fca357f9d6faafe29da2c7e0d3cecf820e6b
piacsek <felipe@piacsek.me>
2024-07-03 20:52:32 -0700

    nvim: adds lualine plugin

--- nvim/lua/piacsek/lazy/gitblame.lua
+++ nvim/lua/piacsek/lazy/gitblame.lua
@@ -1,3 +1,8 @@
 return {
 	"f-person/git-blame.nvim",
+	config = function()
+		require("gitblame").setup({
+			date_format = "%r",
+		})
+	end,
 }
---
+++ nvim/lua/piacsek/lazy/lualine.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+		local git_blame = require("gitblame")
+
+		require("lualine").setup({
+			sections = {
+				lualine_b = {
+					{ "filename", "diff", "diagnostics" },
+				},
+				lualine_c = {
+					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+				},
+			},
+		})
+	end,
+}
