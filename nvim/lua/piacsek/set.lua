ecb3a87d0665a60a51eca59cf623b51a920c7e2f
piacsek <felipe@piacsek.me>
2024-07-03 21:13:33 -0700

    nvim: adjusts tabstop and shiftwidth

--- nvim/lua/piacsek/set.lua
+++ nvim/lua/piacsek/set.lua
@@ -3,11 +3,11 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.showmode = false
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.breakindent = true
 
@@ -16,7 +16,7 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 250
 
@@ -26,12 +26,15 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
 
 vim.opt.hlsearch = true
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
