cf36753636b3c44d8dff83aa65b6d95d6748c95c
piacsek <felipe@piacsek.me>
2025-08-14 19:26:38 -0700

    config: add spacing between neotest keymap definitions

--- nvim/lua/piacsek/remap.lua
+++ nvim/lua/piacsek/remap.lua
@@ -99,15 +99,19 @@ vim.keymap.set("t", "<C-Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<leader>tt", function()
 	require("neotest").run.run()
 end, { desc = "[T]est neares[T]" })
+
 vim.keymap.set("n", "<leader>to", function()
 	require("neotest").output_panel.open()
 end, { desc = "[T]est [O]utput" })
+
 vim.keymap.set("n", "<leader>tf", function()
 	require("neotest").run.run(vim.fn.expand("%"))
 end, { desc = "[T]est [F]ile" })
+
 vim.keymap.set("n", "<leader>ts", function()
 	require("neotest").summary.toggle()
 end, { desc = "[T]est [S]ummary" })
+
 vim.keymap.set("n", "<leader>to", function()
 	require("neotest").output.open({ enter = true })
 end, { desc = "[T]est [O]utput" })
