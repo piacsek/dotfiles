9ac4490adbc02be1fc2d2bd0db3fa433049e0c69
piacsek <felipe@piacsek.me>
2025-08-15 17:39:00 -0700

    config: remove unused lazy.nvim install colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,9 +90,6 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
