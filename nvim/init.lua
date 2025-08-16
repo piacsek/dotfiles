905e174b64e187034d80d524cda88767998a3fef
piacsek <felipe@piacsek.me>
2025-08-15 18:35:27 -0700

    config: fix neotest adapter require syntax

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,24 +37,24 @@ require("lazy").setup({
     end,
   },
   {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "jfpedroza/neotest-elixir",
-    },
-    lazy = false,
-    config = function()
-      require("neotest").setup({
-        log_level = vim.log.levels.DEBUG,
-        adapters = {
-          require("neotest-elixir"),
-        },
-      })
-    end,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "jfpedroza/neotest-elixir",
   },
+  lazy = false,
+  config = function()
+    require("neotest").setup({
+      log_level = vim.log.levels.DEBUG,
+      adapters = {
+        ["neotest-elixir"] = {},
+      },
+    })
+  end,
+},
 })
 
 
