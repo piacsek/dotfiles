d2867bcd81f5dd7141ad9e43ec7b3b358d00b60f
piacsek <felipe@piacsek.me>
2025-08-16 11:30:46 -0700

    config: uncomment adapter lua path in neotest

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,8 +50,8 @@ require("lazy").setup({
     local adapter_dir = vim.fn.stdpath("data") .. "/lazy/neotest-elixir"
 
     local lua_paths = table.concat({
-      -- adapter_dir .. "/lua/?.lua",
-      adapter_dir .. "/lua/?/init.lua",
+      adapter_dir .. "/lua/?.lua",
+      -- adapter_dir .. "/lua/?/init.lua",
     }, ";")
 
     if not vim.env.LUA_PATH or #vim.env.LUA_PATH <= 0 then
