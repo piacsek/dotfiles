c303e84e5603809073dc9c0299146ba338f03596
piacsek <felipe@piacsek.me>
2025-08-15 17:24:47 -0700

    config: alphabetize vim.opt settings with spacing

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,29 +12,40 @@ vim.g.maplocalleader = " "
 
 -- vim.opt configs
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
+vim.opt.autoread = true
+
 vim.opt.breakindent = true
-vim.opt.undofile = true
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+
+vim.opt.hlsearch = true
+
 vim.opt.ignorecase = false
-vim.opt.smartcase = false
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.inccommand = "split"
+
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
+
+vim.opt.mouse = "a"
+
+vim.opt.number = true
+
+vim.opt.relativenumber = true
+
 vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
 vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = false
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 vim.opt.tabstop = 2
-vim.opt.autoread = true
+vim.opt.timeoutlen = 300
+
+vim.opt.updatetime = 250
+vim.opt.undofile = true
 
 -- vim.api configs
 
