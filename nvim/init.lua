f7947ffcbaf9a52f82526a2133cb3f0730646a2e
piacsek <felipe@piacsek.me>
2025-08-18 18:11:37 -0700

    [2025-08-18 18:11:37] Claude failed to generate commit message, using fallback Auto-sync dotfiles - 2025-08-18 18:11:37

--- nvim/init.lua
+++ nvim/init.lua
@@ -368,25 +368,25 @@ require("lazy").setup({
 			})
 		end,
 	},
-	return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
-		local git_blame = require("gitblame")
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+			local git_blame = require("gitblame")
 
-		require("lualine").setup({
-			sections = {
-				lualine_b = {
-					{ "filename", "diff", "diagnostics" },
+			require("lualine").setup({
+				sections = {
+					lualine_b = {
+						{ "filename", "diff", "diagnostics" },
+					},
+					lualine_c = {
+						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+					},
 				},
-				lualine_c = {
-					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
-				},
-			},
-		})
-	end,
-}
+			})
+		end,
+	}
 
 
 
