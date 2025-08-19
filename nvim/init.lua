ab734908e4131dd33d5fe61d966500a62d63c6c5
piacsek <felipe@piacsek.me>
2025-08-18 18:44:06 -0700

    config: fix harpoon requires in keybinding functions

--- nvim/init.lua
+++ nvim/init.lua
@@ -630,11 +630,11 @@ end
 
 -- <Harpoon remaps>
 vim.keymap.set("n", "<leader>e", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
+	require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
 end)
 
 vim.keymap.set("n", "<leader>a", function()
-	harpoon:list():add()
+	require("harpoon"):list():add()
 end)
 
 local keys_that_toggle_selections = { "j", "k", "l", "h" }
@@ -642,17 +642,17 @@ local keys_that_toggle_selections = { "j", "k", "l", "h" }
 for file_index, key in ipairs(keys_that_toggle_selections) do
 	local open_on_split_buffer_remap = ("<C-S-%s>"):format(key)
 	vim.keymap.set("n", open_on_split_buffer_remap, function()
-		harpoon:list():select(file_index, { vsplit = true })
+		require("harpoon"):list():select(file_index, { vsplit = true })
 	end)
 
 	local open_on_same_buffer_remap = ("<C-%s>"):format(key)
 	vim.keymap.set("n", open_on_same_buffer_remap, function()
-		harpoon:list():select(file_index)
+		require("harpoon"):list():select(file_index)
 	end)
 
 	local replace_file_remap = ("<leader><C-%s>"):format(key)
 	vim.keymap.set("n", replace_file_remap, function()
-		harpoon:list():select(file_index)
+		require("harpoon"):list():select(file_index)
 	end)
 end
 
