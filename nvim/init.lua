d449541d0550021cbd5afcff3a2d13f41a1bfbbe
piacsek <felipe@piacsek.me>
2025-08-18 18:23:06 -0700

    config: add global get_cmp_mappings function

--- nvim/init.lua
+++ nvim/init.lua
@@ -495,6 +495,30 @@ end, { desc = "[F]find [N]eovim files" })
 
 -- </Telescope remaps>
 
+-- <CMP remaps>
+function _G.get_cmp_mappings(cmp, luasnip)
+	return cmp.mapping.preset.insert {
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-y>'] = cmp.mapping.confirm { select = true },
+		['<C-Space>'] = cmp.mapping.complete {},
+		['<C-l>'] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { 'i', 's' }),
+
+		['<C-h>'] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { 'i', 's' }),
+	}
+end
+-- </CMP remaps>
+
 -- <LSP remaps>
 function _G.setup_lsp_keymaps(event)
 	local map = function(keys, func, desc)
