51dcd7774069ec533b4f13dd50b27d9a278ca3cf
piacsek <felipe@piacsek.me>
2025-08-18 18:15:18 -0700

    config: add global LSP keymap setup function

--- nvim/init.lua
+++ nvim/init.lua
@@ -509,7 +509,31 @@ end, { desc = "[F]find [N]eovim files" })
 
 -- </Telescope remaps>
 
+-- <LSP remaps>
+function _G.setup_lsp_keymaps(event)
+	local map = function(keys, func, desc)
+		vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+	end
 
+	map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+	map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+	map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+	map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	map("K", vim.lsp.buf.hover, "Hover Documentation")
+	map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+	local client = vim.lsp.get_client_by_id(event.data.client_id)
+	if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+		map("<leader>th", function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+		end, "[T]oggle Inlay [H]ints")
+	end
+end
+-- </LSP remaps>
 
 ----------------------
 --    </remaps>     --
