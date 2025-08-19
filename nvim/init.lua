883a3b46b5d9ccdc448cf433b0fc932be2c0441a
piacsek <felipe@piacsek.me>
2025-08-18 18:22:45 -0700

    config: extract cmp mappings to global function

--- nvim/init.lua
+++ nvim/init.lua
@@ -221,26 +221,7 @@ require("lazy").setup({
 					end,
 				},
 				completion = { completeopt = 'menu,menuone,noinsert' },
-
-				mapping = cmp.mapping.preset.insert {
-					['<C-n>'] = cmp.mapping.select_next_item(),
-					['<C-p>'] = cmp.mapping.select_prev_item(),
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-y>'] = cmp.mapping.confirm { select = true },
-					['<C-Space>'] = cmp.mapping.complete {},
-					['<C-l>'] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { 'i', 's' }),
-
-					['<C-h>'] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { 'i', 's' }),
-				},
+				mapping = _G.get_cmp_mappings(cmp, luasnip),
 				sources = {
 					{ name = 'nvim_lsp' },
 					{ name = 'luasnip' },
