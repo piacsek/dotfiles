01dfa38fb40f790d3df959eec334574885c54ecf
piacsek <felipe@piacsek.me>
2025-08-20 17:36:47 -0700

    config: remove BS & Del key arrow warning mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -668,14 +668,11 @@ vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = 
 vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
 vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
 vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
-vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
-vim.api.nvim_set_keymap("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
 
 vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<BS>", msg, { noremap = true, silent = false })
 --
 --
 --
