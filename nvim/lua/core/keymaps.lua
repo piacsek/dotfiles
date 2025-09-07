6f77623fcd7c17c86006da62055662ee86dd277d
piacsek <felipe@piacsek.me>
2025-09-07 11:25:03 -0700

    config: add keymap to close neotest summary & nvim tree

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -36,3 +36,8 @@ vim.keymap.set("n", "<", ":vertical resize -5<CR>", { desc = "Decrease window ve
 vim.keymap.set("v", "<leader>s", '"sy:%s/<C-r>s/', { desc = "[S]ubstitute selected word" })
 
 vim.keymap.set("t", "<C-Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set("n", "<leader><CR>", function()
+	require("neotest").summary.close()
+	vim.cmd(":NvimTreeClose")
+end, { desc = "[T]est [S]ummary" })
