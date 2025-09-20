bceabc366b85d6465d81775bd213e7f012ce4a04
piacsek <felipe@piacsek.me>
2025-09-19 19:37:46 -0700

    config: simplify stickybuf to use default setup

--- nvim/lua/plugins/stickybuf.lua
+++ nvim/lua/plugins/stickybuf.lua
@@ -1,11 +1,4 @@
 return {
 	"stevearc/stickybuf.nvim",
 	opts = {},
-	config = function()
-		require("stickybuf").setup({
-			get_auto_pin = function(bufnr)
-				return require("stickybuf").should_auto_pin(bufnr)
-			end,
-		})
-	end,
 }
