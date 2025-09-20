a577402dbb729738aedcbac13c0be7d41e3cc5be
piacsek <felipe@piacsek.me>
2025-09-19 19:36:30 -0700

    config: add stickybuf plugin for buffer pinning

---
+++ nvim/lua/plugins/stickybuf.lua
@@ -0,0 +1,11 @@
+return {
+	"stevearc/stickybuf.nvim",
+	opts = {},
+	config = function()
+		require("stickybuf").setup({
+			get_auto_pin = function(bufnr)
+				return require("stickybuf").should_auto_pin(bufnr)
+			end,
+		})
+	end,
+}
