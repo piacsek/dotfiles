d24310291a06200146b9430ec82095585db0d20a
piacsek <felipe@piacsek.me>
2025-10-06 15:08:53 -0700

    config: remove git blame from lualine status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,6 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		vim.g.gitblame_display_virtual_text = 0
-		local git_blame = require("gitblame")
 
 		require("lualine").setup({
 			options = {
@@ -15,10 +14,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{ "filename", path = 1 },
-					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
-				},
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
