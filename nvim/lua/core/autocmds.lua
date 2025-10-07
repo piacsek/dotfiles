bd1e14fe76cdb6fbf1b9ab4a121b823d2f0fa93a
piacsek <felipe@piacsek.me>
2025-10-06 19:10:24 -0700

    config: highlight file paths in terminal buffers

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -28,3 +28,32 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.winhighlight = "EndOfBuffer:Normal,VertSplit:Normal"
 	end,
 })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = function()
+		local buf = vim.api.nvim_get_current_buf()
+		local ns = vim.api.nvim_create_namespace("file_path_highlight")
+
+		-- Define highlight group for file paths
+		vim.api.nvim_set_hl(0, "TermFilePath", { bold = true, underline = true })
+
+		-- Create autocmd to highlight paths whenever terminal updates
+		vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI", "TermEnter" }, {
+			buffer = buf,
+			callback = function()
+				vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+				local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+				for i, line in ipairs(lines) do
+					-- Match file paths like lib/my/file.ex or src/foo/bar.rb
+					for match in line:gmatch("[%w_%-%.]+/[%w_%-%.%/]+%.%w+") do
+						local start_col = line:find(match, 1, true)
+						if start_col then
+							vim.api.nvim_buf_add_highlight(buf, ns, "TermFilePath", i - 1, start_col - 1, start_col - 1 + #match)
+						end
+					end
+				end
+			end,
+		})
+	end,
+})
