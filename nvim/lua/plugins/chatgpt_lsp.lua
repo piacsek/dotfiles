de9eb5f827e38de0e2c9b1294eac4ce92ce64b78
piacsek <felipe@piacsek.me>
2025-10-20 18:49:01 -0700

    config: refactor elixirls setup with inlay hints

--- nvim/lua/plugins/chatgpt_lsp.lua
+++ nvim/lua/plugins/chatgpt_lsp.lua
@@ -1,51 +1,67 @@
--- ~/.config/nvim/lua/plugins/elixir-lsp.lua
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- Setup Mason to manage LSP installations
-		require("mason").setup()
-		require("mason-tool-installer").setup({
-			ensure_installed = { "elixir-ls" },
-		})
+	{
+		-- Core LSP client
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPost", "BufNewFile" },
 
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			vim.lsp.protocol.make_client_capabilities(),
-			require("cmp_nvim_lsp").default_capabilities()
-		)
+		dependencies = {
+			{ "williamboman/mason.nvim", config = true },
+			"williamboman/mason-lspconfig.nvim",
+		},
 
-		require("lspconfig").elixirls.setup({
-			cmd = { "elixir-ls" },
-			root_dir = require("lspconfig.util").root_pattern("mix.exs"),
-			capabilities = capabilities,
-			settings = {
-				elixirLS = {
-					dialyzerEnabled = false,
-					fetchDeps = false,
-					enableTestLenses = false,
-					suggestSpecs = false,
-					mixEnv = "dev",
+		config = function()
+			-- Mason: manage LSP binaries
+			require("mason").setup({})
+
+			-- Install ONLY elixirls
+			require("mason-lspconfig").setup({
+				ensure_installed = { "elixirls" },
+				automatic_installation = false,
+			})
+
+			-- Minimal capabilities (no cmp dependency)
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			-- Keymaps on attach (Neovim 0.11 style)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(ev)
+					local map = function(lhs, rhs, desc)
+						vim.keymap.set("n", lhs, rhs, { buffer = ev.buf, desc = "LSP: " .. desc })
+					end
+					map("gd", vim.lsp.buf.definition, "Goto Definition")
+					map("gr", vim.lsp.buf.references, "Goto References")
+					map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+					map("K", vim.lsp.buf.hover, "Hover")
+					map("<leader>rn", vim.lsp.buf.rename, "Rename")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+
+					-- Inlay hints toggle (0.11 API)
+					if vim.lsp.inlay_hint then
+						map("<leader>th", function()
+							local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = ev.buf })
+							vim.lsp.inlay_hint.enable(not enabled, { bufnr = ev.buf })
+						end, "Toggle Inlay Hints")
+					end
+				end,
+			})
+
+			-- Set up elixirls with Dialyzer disabled
+			local lspconfig = require("lspconfig")
+			lspconfig.elixirls.setup({
+				capabilities = capabilities,
+				cmd = { "elixir-ls" }, -- resolved by Mason
+				root_dir = require("lspconfig.util").root_pattern("mix.exs", ".git"),
+				settings = {
+					elixirLS = {
+						dialyzerEnabled = false,
+						suggestSpecs = false,
+						fetchDeps = false,
+						enableTestLenses = false,
+						mixEnv = "dev",
+					},
 				},
-			},
-		})
-
-		-- Basic LSP keymaps
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(event)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-				end
-				map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-				map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-				map("K", vim.lsp.buf.hover, "Hover Documentation")
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-			end,
-		})
-	end,
+			})
+		end,
+	},
 }
