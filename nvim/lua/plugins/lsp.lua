e5614e6b6df0e18defe22c08f92a58821826ee0c
piacsek <felipe@piacsek.me>
2025-10-20 18:34:40 -0700

    config: add ensure_installed to mason-lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -100,6 +100,7 @@ return {
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
+			ensure_installed = ensure_installed,
 			handlers = {
 				function(server_name)
 					local server = servers[server_name] or {}
