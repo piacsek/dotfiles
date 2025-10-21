235aa8a0b56718946412a39b1826405cfd9d6821
piacsek <felipe@piacsek.me>
2025-10-20 18:53:05 -0700

    config: add debug print for LSP server setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -104,6 +104,7 @@ return {
 				function(server_name)
 					local server = servers[server_name] or {}
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					print(server)
 					require("lspconfig")[server_name].setup(server)
 				end,
 			},
