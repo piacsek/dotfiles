b5cb306a54da67bef5ba651c708e63696d4e9ced
piacsek <felipe@piacsek.me>
2025-11-17 10:19:20 -0800

    config: set gitsigns blame delay to 0ms

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,7 +10,7 @@ return {
 		},
 		current_line_blame = true,
 		current_line_blame_opts = {
-			delay = 100,
+			delay = 0,
 			virt_text = true,
 			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 		},
