599c47854f3aec31539cdc9a6383b8fcd6a48c98
piacsek <felipe@piacsek.me>
2025-11-17 15:55:17 -0800

    config: enable html formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -28,7 +28,7 @@ return {
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			json = { "prettierd", "prettier", stop_after_first = true },
 			css = { "prettierd", "prettier", stop_after_first = true },
-			-- html = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
