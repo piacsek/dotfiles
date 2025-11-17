cb91e84145dd0cda599d7a46589a6fddb4571758
piacsek <felipe@piacsek.me>
2025-11-17 15:18:05 -0800

    config: disable format on save for heex files

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,7 +14,7 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true }
+			local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true, heex = true }
 			return {
 				timeout_ms = 3000,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
