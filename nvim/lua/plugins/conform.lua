3ed1e84d1139245757562d6fce8fd36ea5cb8f94
piacsek <felipe@piacsek.me>
2025-11-17 15:52:37 -0800

    config: enable heex format on save, disable html

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,7 +14,7 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true, heex = true }
+			local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true }
 			return {
 				timeout_ms = 3000,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -28,7 +28,7 @@ return {
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			json = { "prettierd", "prettier", stop_after_first = true },
 			css = { "prettierd", "prettier", stop_after_first = true },
-			html = { "prettierd", "prettier", stop_after_first = true },
+			-- html = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
