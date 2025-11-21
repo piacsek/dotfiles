09a6991d769e25b421fac5c143503e6a264a5fb9
piacsek <felipe@piacsek.me>
2025-11-20 19:18:58 -0800

    config: simplify spectre keybindings to <leader>S

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -5,33 +5,19 @@ return {
 	},
 	keys = {
 		{
-			"<leader>SS",
+			"<leader>S",
 			function()
 				require("spectre").toggle()
 			end,
 			desc = "Toggle Spectre",
 		},
 		{
-			"<leader>SW",
-			function()
-				require("spectre").open_visual({ select_word = true })
-			end,
-			desc = "Search current word",
-		},
-		{
-			"<leader>SW",
+			"<leader>S",
 			mode = "v",
 			function()
 				require("spectre").open_visual()
 			end,
 			desc = "Search current word",
-		},
-		{
-			"<leader>S/",
-			function()
-				require("spectre").open_file_search({ select_word = true })
-			end,
-			desc = "Search in current file",
 		},
 	},
 	opts = {},
