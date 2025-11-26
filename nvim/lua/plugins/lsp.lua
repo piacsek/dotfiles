f09c69eb85a1088cef9b123794a3d837ab41899d
piacsek <felipe@piacsek.me>
2025-11-25 17:25:51 -0800

    config: refactor elixirls to use dedicated handler

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -134,12 +134,31 @@ return {
 					-- Ensure capabilities are properly merged
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 
-					-- Debug logging to verify settings are being passed
-					if server_name == "elixirls" then
-						vim.notify("Setting up elixirls with settings: " .. vim.inspect(server.settings), vim.log.levels.INFO)
-					end
-
 					require("lspconfig")[server_name].setup(server)
+				end,
+				-- Specific handler for elixirls to ensure settings are applied
+				elixirls = function()
+					require("lspconfig").elixirls.setup({
+						capabilities = capabilities,
+						filetypes = { "elixir", "eelixir", "heex", "surface" },
+						root_dir = require("lspconfig.util").root_pattern("mix.exs"),
+						init_options = {
+							dialyzerEnabled = false,
+							fetchDeps = false,
+							enableTestLenses = false,
+							suggestSpecs = false,
+							mixEnv = "dev",
+						},
+						settings = {
+							elixirLS = {
+								dialyzerEnabled = false,
+								fetchDeps = false,
+								enableTestLenses = false,
+								suggestSpecs = false,
+								mixEnv = "dev",
+							},
+						},
+					})
 				end,
 			},
 		})
