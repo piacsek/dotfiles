0d168301ca81045323f7588ff73e994a70aefe99
piacsek <felipe@piacsek.me>
2025-11-29 16:09:45 -0800

    config: simplify <leader><Esc> to always close buffer

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -19,12 +19,8 @@ vim.keymap.set("n", "<C-g>", "#*viw", { desc = "Multiple cursor replacement" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search results highlights" })
 
 vim.keymap.set("n", "<leader><Esc>", function()
-	if vim.bo.buftype == "terminal" then
-		vim.cmd("hide")
-	else
-		vim.cmd("bd")
-	end
-end, { desc = "Hide window or close buffer" })
+	vim.cmd("bd")
+end, { desc = "Close buffer" })
 vim.keymap.set("n", "Y", "y$", { desc = "[Y]ank till the end of the line" })
 vim.keymap.set("n", "V", "v$", { desc = "[V]isually select till the end of the line" })
 
