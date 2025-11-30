5f35e057707778ce7cc689e586ae8342ba1f2a28
piacsek <felipe@piacsek.me>
2025-11-29 16:22:23 -0800

    config: restore line numbers after buffer delete

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -22,6 +22,8 @@ vim.keymap.set("n", "<leader><Esc>", function()
 	local type = vim.bo.buftype
 	if type == "" or type == "nofile" or type == "help" then
 		vim.cmd("bd")
+		vim.opt.number = true
+		vim.opt.relativenumber = true
 	end
 end, { desc = "Delete buffer" })
 vim.keymap.set("n", "Y", "y$", { desc = "[Y]ank till the end of the line" })
