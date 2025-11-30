995f7d943c52d84aa84a7c18ccedc01718d060ab
piacsek <felipe@piacsek.me>
2025-11-29 16:07:35 -0800

    config: change <leader><Esc> to close buffer or hide terminal

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -19,11 +19,12 @@ vim.keymap.set("n", "<C-g>", "#*viw", { desc = "Multiple cursor replacement" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search results highlights" })
 
 vim.keymap.set("n", "<leader><Esc>", function()
-	local buffers = vim.fn.getbufinfo({ buflisted = 1 })
-	if #buffers > 1 then
+	if vim.bo.buftype == "terminal" then
 		vim.cmd("hide")
+	else
+		vim.cmd("bd")
 	end
-end, { desc = "Hide window" })
+end, { desc = "Hide window or close buffer" })
 vim.keymap.set("n", "Y", "y$", { desc = "[Y]ank till the end of the line" })
 vim.keymap.set("n", "V", "v$", { desc = "[V]isually select till the end of the line" })
 
