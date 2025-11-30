0dc670f2eb1fea80539158618a459ddfca2a31e6
piacsek <felipe@piacsek.me>
2025-11-29 16:15:42 -0800

    config: fix buffer close check for empty buftype

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search resu
 
 vim.keymap.set("n", "<leader><Esc>", function()
 	vim.notify(vim.bo.buftype, vim.log.levels.INFO)
-	if vim.bo.buftype ~= "terminal" then
+	if vim.bo.buftype == nil then
 		vim.cmd("bd")
 	end
 end, { desc = "Hide window or close buffer" })
