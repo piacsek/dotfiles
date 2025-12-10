5b293b29b70a2d988c735e13e83e759cf244ae63
piacsek <felipe@piacsek.me>
2025-12-09 17:27:26 -0800

    config: disable line wrapping

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -13,6 +13,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.mouse = "a"
 vim.opt.number = true
+vim.opt.nowrap = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 vim.opt.sidescrolloff = 999
