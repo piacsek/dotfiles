1a1892fdcaf7c30c3c6e0b5d868b797f1fc2ae17
piacsek <felipe@piacsek.me>
2025-12-11 12:33:28 -0800

    config: remove nowrap option

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -13,7 +13,6 @@ vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.mouse = "a"
 vim.opt.number = true
-vim.opt.nowrap = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 vim.opt.sidescrolloff = 999
