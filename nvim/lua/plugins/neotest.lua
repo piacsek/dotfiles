01f299462f7e574a5a130f3748b3f8447e084496
piacsek <felipe@piacsek.me>
2025-12-17 13:38:00 -0800

    config: inline neotest keymaps into config function

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,31 +1,3 @@
-local function setup_keymaps(neotest)
-	vim.keymap.set("n", "<leader>tt", function()
-		neotest.run.run()
-	end, { desc = "[T]est neares[T]" })
-
-	vim.keymap.set("n", "<leader>tf", function()
-		neotest.run.run(vim.fn.expand("%"))
-	end, { desc = "[T]est [F]ile" })
-
-	vim.keymap.set("n", "<leader>ts", function()
-		neotest.summary.toggle()
-	end, { desc = "[T]est [S]ummary" })
-
-	vim.keymap.set("n", "<leader>to", function()
-		neotest.output.open({ enter = true })
-	end, { desc = "[T]est [O]utput" })
-
-	vim.keymap.set("n", "<leader><BS>", function()
-		if vim.bo.buftype == "" then
-			vim.cmd("w")
-		end
-		local position_id, last_args = neotest.run.get_last_run()
-		if position_id and last_args then
-			neotest.run.run_last()
-		end
-	end, { desc = "Save file and re-run last test (if any)" })
-end
-
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
@@ -68,6 +40,30 @@ return {
 			},
 		})
 
-		setup_keymaps(neotest)
+		vim.keymap.set("n", "<leader>tt", function()
+			neotest.run.run()
+		end, { desc = "[T]est neares[T]" })
+
+		vim.keymap.set("n", "<leader>tf", function()
+			neotest.run.run(vim.fn.expand("%"))
+		end, { desc = "[T]est [F]ile" })
+
+		vim.keymap.set("n", "<leader>ts", function()
+			neotest.summary.toggle()
+		end, { desc = "[T]est [S]ummary" })
+
+		vim.keymap.set("n", "<leader>to", function()
+			neotest.output.open({ enter = true })
+		end, { desc = "[T]est [O]utput" })
+
+		vim.keymap.set("n", "<leader><BS>", function()
+			if vim.bo.buftype == "" then
+				vim.cmd("w")
+			end
+			local position_id, last_args = neotest.run.get_last_run()
+			if position_id and last_args then
+				neotest.run.run_last()
+			end
+		end, { desc = "Save file and re-run last test (if any)" })
 	end,
 }
