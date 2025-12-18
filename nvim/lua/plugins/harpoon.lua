f1931a31e00e27734e5d90a1517117703b22b0fc
piacsek <felipe@piacsek.me>
2025-12-18 12:48:56 -0800

    config: enable gitsigns on harpoon file select

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -17,6 +17,7 @@ local function setup_keymaps(harpoon)
 
 	for file_index, key in ipairs({ "j", "k", "l", "h" }) do
 		vim.keymap.set("n", ("<C-%s>"):format(key), function()
+			vim.cmd("Gitsigns attach")
 			harpoon:list():select(file_index)
 		end)
 	end
