f27668f1585407d6ac8036fbc5f6b7372dc4e555
piacsek <felipe@piacsek.me>
2025-12-19 17:45:15 -0800

    config: add global templates and remove debug logs

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -24,12 +24,14 @@ return {
 			},
 		})
 
+		local global_templates = vim.fn.stdpath("config") .. "/lua/overseer/templates.lua"
+		if vim.fn.filereadable(global_templates) == 1 then
+			dofile(global_templates)(overseer)
+		end
+
 		local project_templates = vim.fn.getcwd() .. "/piacsek/overseer/templates.lua"
 		if vim.fn.filereadable(project_templates) == 1 then
-			vim.notify("Loading: " .. project_templates)
 			dofile(project_templates)(overseer)
-		else
-			vim.notify("Not found: " .. project_templates)
 		end
 	end,
 	keys = {
