7d3b89e9e78c3eab7e1c9f2296d0f80fb6729fa9
piacsek <felipe@piacsek.me>
2025-12-22 11:26:27 -0800

    config: remove custom lspkind formatting from cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -42,14 +42,6 @@ return {
 			},
 			formatting = {
 				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-					local strings = vim.split(kind.kind, "%s", { trimempty = true })
-					kind.kind = " " .. (strings[1] or "") .. " "
-					kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-					return kind
-				end,
 			},
 			snippet = {
 				expand = function(args)
