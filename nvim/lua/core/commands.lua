5e538b10948fe05684b455018878f4f89f69d1b1
piacsek <felipe@piacsek.me>
2025-12-23 15:18:39 -0800

    config: add auto-cleanup timer for hidden buffers

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -87,7 +87,7 @@ vim.api.nvim_create_user_command("BufferStats", function()
 end, { desc = "Show buffer statistics" })
 
 -- Auto-cleanup hidden buffers that haven't been used in a while
-vim.api.nvim_create_user_command("CleanHiddenBuffers", function()
+local function clean_hidden_buffers(silent)
 	local current = vim.api.nvim_get_current_buf()
 	local deleted = 0
 
@@ -105,5 +105,23 @@ vim.api.nvim_create_user_command("CleanHiddenBuffers", function()
 		end
 	end
 
-	vim.notify(string.format("Cleaned %d hidden buffers", deleted), vim.log.levels.INFO)
+	if not silent then
+		vim.notify(string.format("Cleaned %d hidden buffers", deleted), vim.log.levels.INFO)
+	end
+
+	return deleted
+end
+
+vim.api.nvim_create_user_command("CleanHiddenBuffers", function()
+	clean_hidden_buffers(false)
 end, { desc = "Delete all unmodified hidden buffers" })
+
+-- Automatic cleanup every 3 minutes (silent)
+local cleanup_timer = vim.uv.new_timer()
+cleanup_timer:start(
+	180000, -- Start after 3 minutes
+	180000, -- Repeat every 3 minutes
+	vim.schedule_wrap(function()
+		clean_hidden_buffers(true)
+	end)
+)
