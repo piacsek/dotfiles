a358ba1dfcdfdcc0e2b134566eaba5444206515f
piacsek <felipe@piacsek.me>
2025-12-27 12:40:32 -0800

    config: simplify oil test to single file under cursor

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,30 +7,19 @@ return {
 			["<leader>tt"] = {
 				callback = function()
 					local oil = require("oil")
-					local dir = oil.get_current_dir()
+					local entry = oil.get_cursor_entry()
 
-					local entries = oil.get_selected_entries()
-					if not entries or #entries == 0 then
-						local entry = oil.get_cursor_entry()
-						if entry then
-							entries = { entry }
-						end
-					end
-
-					local test_files = {}
-					for _, entry in ipairs(entries) do
-						local filepath = dir .. entry.name
-						table.insert(test_files, filepath)
-					end
-
-					if #test_files == 0 then
-						vim.notify("No test files selected", vim.log.levels.WARN)
+					if not entry then
+						vim.notify("No file under cursor", vim.log.levels.WARN)
 						return
 					end
 
-					vim.cmd("TestSuite " .. table.concat(test_files, " "))
+					local dir = oil.get_current_dir()
+					local filepath = dir .. entry.name
+
+					vim.cmd("TestSuite " .. filepath)
 				end,
-				desc = "Run test file(s) under cursor/selection",
+				desc = "Run test file under cursor",
 			},
 		},
 	},
