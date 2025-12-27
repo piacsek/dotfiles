3db4499cdf7579701d5c6b41a74d25a4929ca21b
piacsek <felipe@piacsek.me>
2025-12-27 15:25:24 -0800

    config: remove gitlinker plugin

--- nvim/lua/plugins/gitlinker.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"linrongbin16/gitlinker.nvim",
-	cmd = "GitLink",
-	opts = {},
-	keys = {
-		{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "[Y]ank [g]it link" },
-		{ "<leader>gw", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open [g]it link on the [w]eb browser" },
-	},
-}
