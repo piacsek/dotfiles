7932f49f269ea45565f2b491b9d2c2d81b0a582c
piacsek <felipe@piacsek.me>
2025-12-27 11:41:59 -0800

    config: replace neotest with vim-test plugin

---
+++ nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,12 @@
+return {
+	"vim-test/vim-test",
+	keys = {
+		{ "<leader>tt", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+		{ "<leader>tf", "<cmd>TestFile<cr>", desc = "Test file" },
+		{ "<leader>tl", "<cmd>TestLast<cr>", desc = "Test last" },
+	},
+	config = function()
+		vim.g["test#strategy"] = "neovim_sticky"
+		vim.g["test#neovim#term_position"] = "vert botright"
+	end,
+}
