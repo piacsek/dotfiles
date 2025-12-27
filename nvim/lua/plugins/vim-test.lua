c4e17c0d66e726976575b289b2d7720f6a92a197
piacsek <felipe@piacsek.me>
2025-12-27 11:53:53 -0800

    config: disable test command echoing in vim-test

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -8,6 +8,7 @@ return {
 	config = function()
 		vim.g["test#strategy"] = "neovim_sticky"
 		vim.g["test#preserve_screen"] = 0
+		vim.g["test#echo_command"] = 0
 		vim.g["test#neovim#term_position"] = "topleft vsplit"
 		vim.g["test#neovim#kill_previous"] = 1
 		vim.g["test#neovim#reopen_window"] = 1
