5d757473f2c232e2e86a6dbdc3105b4614fec7f7
piacsek <felipe@piacsek.me>
2025-12-27 16:21:14 -0800

    config: add neovim news viewer keybinding

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -114,5 +114,23 @@ return {
 			end,
 			desc = "Lazygit",
 		},
+		{
+			"<leader>N",
+			desc = "Neovim News",
+			function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end,
+		},
 	},
 }
