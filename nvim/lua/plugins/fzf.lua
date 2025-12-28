56d641eddfe5d23d6ed44141ed31a2c2909613b1
piacsek <felipe@piacsek.me>
2025-12-28 14:09:17 -0800

    config: remove comments and simplify regex escape

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -19,14 +19,12 @@ local function setup_keymaps()
 
 	function vim.getVisualSelectionEscaped()
 		local text = vim.getVisualSelection()
-		-- Escape special regex characters for grep
-		text = vim.fn.escape(text, "()[]{}.*+?^$|\\")
+		-- text = vim.fn.escape(text, "()[]{}.*+?^$|\\")
 		return text
 	end
 
 	local fzf = require("fzf-lua")
 
-	-- Find files
 	vim.keymap.set("n", "<leader>ff", function()
 		fzf.files()
 	end, { desc = "[F]ind [F]iles" })
@@ -36,27 +34,22 @@ local function setup_keymaps()
 		fzf.files({ query = text })
 	end, { noremap = true, silent = true, desc = "[F]ind [F]files with selected text" })
 
-	-- Help tags
 	vim.keymap.set("n", "<leader>fh", function()
 		fzf.help_tags()
 	end, { desc = "[F]ind [H]elp" })
 
-	-- Colorscheme
 	vim.keymap.set("n", "<leader>ft", function()
 		fzf.colorschemes()
 	end, { desc = "[F]ind [T]heme" })
 
-	-- Keymaps
 	vim.keymap.set("n", "<leader>fk", function()
 		fzf.keymaps()
 	end, { desc = "[F]ind [K]eymaps" })
 
-	-- Live grep
 	vim.keymap.set("n", "<leader>fg", function()
 		fzf.live_grep({ winopts = grep_winopts })
 	end, { desc = "[F]ind by [G]rep" })
 
-	-- Resume
 	vim.keymap.set("n", "<leader>fr", function()
 		fzf.resume()
 	end, { desc = "[F]ind [R]esume" })
@@ -65,42 +58,30 @@ local function setup_keymaps()
 		fzf.resume()
 	end, { desc = "Resume last search" })
 
-	-- Buffers
-	vim.keymap.set("n", "<leader>fb", function()
-		fzf.buffers()
-	end, { desc = "[F]ind [B]uffer" })
-
-	-- Recent files
 	vim.keymap.set("n", "<leader><leader>", function()
-		fzf.oldfiles()
+		fzf.buffers()
 	end, { desc = "[F]ind Recent Files" })
 
-	-- Git history (buffer commits)
 	vim.keymap.set("n", "<leader>gh", function()
 		fzf.git_bcommits()
 	end, { desc = "[G]it [H]istory" })
 
-	-- Current buffer fuzzy find
 	vim.keymap.set("n", "<leader>/", function()
 		fzf.lgrep_curbuf()
 	end, { desc = "[/] Fuzzily search in current buffer" })
 
-	-- Find in piacsek directory
 	vim.keymap.set("n", "<leader>fp", function()
 		fzf.files({ cwd = vim.fn.getcwd() .. "/piacsek" })
 	end, { desc = "[F]ind [P]iacsek files" })
 
-	-- Find in dotfiles
 	vim.keymap.set("n", "<leader>fd", function()
 		fzf.files({ cwd = "~/dotfiles" })
 	end, { desc = "[F]ind [D]otfiles" })
 
-	-- Grep in dotfiles
 	vim.keymap.set("n", "<leader>fc", function()
 		fzf.live_grep({ cwd = "~/dotfiles", winopts = grep_winopts })
 	end, { desc = "Grep config files" })
 
-	-- Find modified git files
 	vim.keymap.set("n", "<leader>fm", function()
 		fzf.git_status()
 	end, { desc = "[F]ind [M]odified git files" })
@@ -110,19 +91,16 @@ local function setup_keymaps()
 		fzf.git_status({ query = text })
 	end, { noremap = true, silent = true, desc = "[F]ind [M]odified git files with selection" })
 
-	-- Visual mode grep
 	vim.keymap.set("v", "<leader>fg", function()
 		local text = vim.getVisualSelectionEscaped()
 		fzf.live_grep({ search = text, winopts = grep_winopts })
 	end, { desc = "[G]rep selected" })
 
-	-- Visual mode buffer search
 	vim.keymap.set("v", "<leader>/", function()
 		local text = vim.getVisualSelectionEscaped()
 		fzf.lgrep_curbuf({ search = text })
 	end, { desc = "[/] Fuzzily search in current buffer" })
 
-	-- Visual mode grep config
 	vim.keymap.set("v", "<leader>fc", function()
 		local text = vim.getVisualSelectionEscaped()
 		fzf.live_grep({ cwd = vim.fn.stdpath("config"), search = text, winopts = grep_winopts })
@@ -134,7 +112,6 @@ return {
 	config = function()
 		local fzf = require("fzf-lua")
 
-		-- Default configuration
 		local config = {
 			winopts = {
 				height = 0.4,
@@ -152,7 +129,6 @@ return {
 			},
 		}
 
-		-- Check for local piacsek/fzf.lua override
 		local cwd = vim.fn.getcwd()
 		local local_config_path = cwd .. "/piacsek/fzf.lua"
 
@@ -165,7 +141,6 @@ return {
 
 		fzf.setup(config)
 
-		-- Use fzf-lua for vim.ui.select
 		fzf.register_ui_select()
 
 		setup_keymaps()
