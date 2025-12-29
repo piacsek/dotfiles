73876e3fdfcfcae74179635a65a9a29b887f1fc7
piacsek <felipe@piacsek.me>
2025-12-29 12:47:08 -0800

    nvim: remove BufOnly command

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -1,16 +1,3 @@
--- Close all buffers except the current one
-vim.api.nvim_create_user_command("BufOnly", function()
-	local current = vim.api.nvim_get_current_buf()
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
-			local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-			if buftype ~= "terminal" then
-				vim.api.nvim_buf_delete(buf, { force = false })
-			end
-		end
-	end
-end, { desc = "Close all buffers except current" })
-
 vim.api.nvim_create_user_command("ClearOldfiles", function()
 	vim.v.oldfiles = {}
 	vim.cmd("wshada!")
