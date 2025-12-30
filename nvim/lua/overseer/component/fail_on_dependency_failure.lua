4e1c622aefff1aa4b1f852cfd81ca7af9335f71a
piacsek <felipe@piacsek.me>
2025-12-30 15:52:04 -0800

    nvim: simplify fail on dep failure component

--- nvim/lua/overseer/component/fail_on_dependency_failure.lua
+++ nvim/lua/overseer/component/fail_on_dependency_failure.lua
@@ -1,4 +1,3 @@
-local task_list = require("overseer.task_list")
 local constants = require("overseer.constants")
 local STATUS = constants.STATUS
 
@@ -9,29 +8,11 @@ M.desc = "Set task status to FAILURE when any dependency fails"
 
 M.constructor = function()
 	return {
-		on_pre_start = function(self, task)
-			-- Check if any dependency has failed
-			local deps_component
-			for _, comp in ipairs(task.components) do
-				if comp.desc and comp.desc:match("^Set dependencies") then
-					deps_component = comp
-					break
-				end
+		on_dependency_complete = function(self, task, dep_task, status)
+			-- If dependency failed or was canceled, mark parent as failed
+			if status == STATUS.FAILURE or status == STATUS.CANCELED then
+				task:set_status(STATUS.FAILURE)
 			end
-
-			if deps_component and deps_component.task_lookup then
-				for _, task_id in ipairs(deps_component.task_lookup) do
-					local dep_task = task_list.get(task_id)
-					if dep_task and (dep_task.status == STATUS.FAILURE or dep_task.status == STATUS.CANCELED) then
-						-- Mark parent as failed and stop
-						task:set_status(STATUS.FAILURE)
-						return false
-					end
-				end
-			end
-
-			-- Let other components proceed
-			return true
 		end,
 	}
 end
