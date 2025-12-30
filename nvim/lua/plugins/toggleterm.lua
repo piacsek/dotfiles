6c525c15abfba179503442ea3be282ec21764e49
piacsek <felipe@piacsek.me>
2025-12-30 14:12:25 -0800

    nvim: add toggleterm.nvim plugin configuration

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,33 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 15
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
+		open_mapping = [[<c-\>]],
+		hide_numbers = true,
+		shade_terminals = false,
+		start_in_insert = true,
+		insert_mappings = true,
+		terminal_mappings = true,
+		persist_size = true,
+		persist_mode = true,
+		direction = "float",
+		close_on_exit = true,
+		shell = vim.o.shell,
+		float_opts = {
+			border = "curved",
+			width = function()
+				return math.floor(vim.o.columns * 0.9)
+			end,
+			height = function()
+				return math.floor(vim.o.lines * 0.9)
+			end,
+		},
+	},
+}
