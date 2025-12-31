b67dca844a5869aa57c358b6a63351b7388712b0
piacsek <felipe@piacsek.me>
2025-12-30 16:17:04 -0800

    nvim: use on_other_task_status for notifier

--- nvim/lua/overseer/component/orchestrator/on_broadcast_update_workflow_notifier.lua
+++ nvim/lua/overseer/component/orchestrator/on_broadcast_update_workflow_notifier.lua
@@ -5,11 +5,14 @@ M.desc = "Forward orchestrator updates to workflow_notifier"
 
 M.constructor = function()
 	return {
-		on_orchestrator_broadcast = function(self, task)
-			-- Find workflow_notifier component and trigger update
-			for _, comp in ipairs(task.components) do
-				if comp.on_orchestrator_update then
-					comp:on_orchestrator_update(task)
+		on_other_task_status = function(self, task, other_task)
+			-- Only process if this is an orchestrator task and other_task is a subtask
+			if task.strategy and task.strategy.name == "orchestrator" then
+				-- Find workflow_notifier component and trigger update
+				for _, comp in ipairs(task.components) do
+					if comp.on_orchestrator_update then
+						comp:on_orchestrator_update(task)
+					end
 				end
 			end
 		end,
