8a40f1bb977fb7bef2e8dff71916def1e2e7603a
piacsek <felipe@piacsek.me>
2026-01-01 13:25:59 -0800

    config: enable opencode go/goo operator keymaps

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -23,11 +23,11 @@ return {
 			require("opencode").command("session.half.page.down")
 		end, { desc = "opencode half page down" })
 
-		-- vim.keymap.set({ "n", "x", "v" }, "go", function()
-		-- 	return require("opencode").operator("@this ")
-		-- end, { expr = true, desc = "Add range to opencode" })
-		-- vim.keymap.set("n", "goo", function()
-		-- 	return require("opencode").operator("@this ") .. "_"
-		-- end, { expr = true, desc = "Add line to opencode" })
+		vim.keymap.set({ "n", "x", "v" }, "go", function()
+			return require("opencode").operator("@this ")
+		end, { expr = true, desc = "Add range to opencode" })
+		vim.keymap.set("n", "goo", function()
+			return require("opencode").operator("@this ") .. "_"
+		end, { expr = true, desc = "Add line to opencode" })
 	end,
 }
