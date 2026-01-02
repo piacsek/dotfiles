36f87d8ed5a08ba28000ceac9b160bd412f2ebfe
piacsek <felipe@piacsek.me>
2026-01-01 18:44:08 -0800

    config: change vim-test strategy to harpoon

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -23,7 +23,8 @@ return {
 		},
 	},
 	config = function()
-		vim.g["test#strategy"] = "neovim_sticky"
+		-- vim.g["test#strategy"] = "neovim_sticky"
+		vim.g["test#strategy"] = "harpoon"
 		vim.g["test#preserve_screen"] = 0
 		vim.g["test#echo_command"] = 0
 		vim.g["test#neovim#term_position"] = "topleft vsplit"
