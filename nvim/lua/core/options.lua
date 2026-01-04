50486c106b7d788739a5875bf761c7f170414b47
piacsek <felipe@piacsek.me>
2026-01-04 14:31:36 -0800

    config: remove scrolloff and sidescrolloff settings

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -16,8 +16,6 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 999
-vim.opt.sidescrolloff = 999
 vim.opt.shiftwidth = 2
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
