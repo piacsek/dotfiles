b7e53d579ba36a71544ede9707bd536fc863e2ae
piacsek <felipe@piacsek.me>
2026-01-04 15:01:21 -0800

    config: set vim-test terminal to left vsplit

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -47,6 +47,7 @@ return {
 		vim.g["test#strategy"] = "neovim_sticky"
 		vim.g["test#preserve_screen"] = 0
 		vim.g["test#echo_command"] = 0
+		vim.g["test#neovim#term_position"] = "topleft vsplit"
 		vim.g["test#neovim_sticky#kill_previous"] = 1
 	end,
 }
