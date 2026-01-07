251ffc2391a7ad2105df246e9e2ab85c710887dd
piacsek <felipe@piacsek.me>
2026-01-07 10:17:30 -0800

    config: extract colorscheme into global variable

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,8 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
-vim.cmd.colorscheme("high-contrast")
+vim.g._default_theme = "high-contrast"
+vim.cmd.colorscheme(vim.g._default_theme)
 
 vim.notify = require("snacks").notifier.notify
 
