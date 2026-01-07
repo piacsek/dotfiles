3c0c2f791853008e5ea8c175532a8eef7c691e72
piacsek <felipe@piacsek.me>
2026-01-07 11:26:29 -0800

    config: fix ThemeDefault to use _default_colorscheme

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -90,6 +90,6 @@ vim.api.nvim_create_user_command("ThemeRandom", function()
 end, { desc = "Assigns a random colorscheme" })
 
 vim.api.nvim_create_user_command("ThemeDefault", function()
-	vim.cmd.colorscheme(vim.g._default_theme)
-	vim.notify(vim.g._default_theme)
+	vim.cmd.colorscheme(vim.g._default_colorscheme)
+	vim.notify(vim.g._default_colorscheme)
 end, { desc = "Assigns the default colorscheme" })
