072d6e88416608426bfcf901397792ee01619995
piacsek <felipe@piacsek.me>
2026-01-13 09:58:45 -0800

    config: remove debug print for local init path

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,7 +31,6 @@ vim.notify = require("snacks").notifier.notify
 
 require("core.lsp")
 local local_init = vim.fn.getcwd() .. "/piacsek/init.lua"
-print(local_init)
 if vim.fn.filereadable(local_init) == 1 then
 	dofile(local_init)
 end
