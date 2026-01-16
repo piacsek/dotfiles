fea4f2f268738be5a83581ef5d5a41b55782ad9c
piacsek <felipe@piacsek.me>
2026-01-16 10:15:49 -0800

    config: remove colorscheme rainbow12 config

--- nvim/lua/plugins/theme-rainbow12.lua
+++ nvim/lua/plugins/theme-rainbow12.lua
@@ -1,6 +1,5 @@
 return {
-    "0Risotto/rainbow12",
-    lazy = false,
-    priority = 1000,
-    config = function() vim.cmd "colorscheme rainbow12" end,
-  },
+	"0Risotto/rainbow12",
+	lazy = false,
+	priority = 1000,
+}
