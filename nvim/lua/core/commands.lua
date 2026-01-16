d27208bf80967f996ec69a753750e8b8dd2d784d
piacsek <felipe@piacsek.me>
2026-01-16 10:17:50 -0800

    config: add rainbow12 to ThemeRandom command

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -83,7 +83,7 @@ end, { desc = "Clear all notifications" })
 
 vim.api.nvim_create_user_command("ThemeRandom", function()
 	-- local all_colorschemes = vim.fn.getcompletion("", "color")
-	local colorschemes = { "high-contrast", "evening", "moonfly", "cyberdream", "nordic" }
+	local colorschemes = { "high-contrast", "evening", "moonfly", "cyberdream", "nordic", "rainbow12" }
 	local random_colorscheme = colorschemes[math.random(#colorschemes)]
 	vim.cmd.colorscheme(random_colorscheme)
 	vim.notify(random_colorscheme)
