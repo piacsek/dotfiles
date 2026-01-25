294ced78ec4f71e2fe6a1deed8dca2706856f99c
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-01-24 20:30:42 -0800

    [2026-01-24 20:30:42] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-01-24 20:30:42

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,51 @@
+local languages = {
+	"bash",
+	"diff",
+	"html",
+	"lua",
+	"javascript",
+	"typescript",
+	"tsx",
+	"luadoc",
+	"markdown",
+	"vim",
+	"vimdoc",
+	"elixir",
+	"heex",
+	"eex",
+	"json",
+}
+
+local filetypes = {
+	"bash",
+	"diff",
+	"html",
+	"lua",
+	"javascript",
+	"typescript",
+	"typescriptreact",
+	"javascriptreact",
+	"markdown",
+	"vim",
+	"elixir",
+	"heex",
+	"eex",
+	"json",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "main",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = {
-			"bash",
-			"diff",
-			"html",
-			"lua",
-			"javascript",
-			"typescript",
-			"tsx",
-			"luadoc",
-			"markdown",
-			"vim",
-			"vimdoc",
-			"elixir",
-			"heex",
-			"eex",
-			"json",
-		},
+		ensure_installed = languages,
 		auto_install = true,
 	},
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"bash",
-				"html",
-				"lua",
-				"javascript",
-				"typescript",
-				"typescriptreact",
-				"javascriptreact",
-				"markdown",
-				"vim",
-				"elixir",
-				"heex",
-				"eex",
-				"json",
-			},
+			pattern = filetypes,
 			callback = function(args)
 				local bufnr = args.buf
 				local filetype = vim.bo[bufnr].filetype
