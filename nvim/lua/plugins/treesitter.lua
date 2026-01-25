9b4fbe7c503201cf0c1b02fbe7b51c94f605b729
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-01-24 20:47:15 -0800

    [2026-01-24 20:47:15] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-01-24 20:47:15

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,25 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"javascript",
-				"typescript",
-				"tsx",
-				"markdown",
-				"markdown_inline",
-				"vim",
-				"vimdoc",
-				"elixir",
-				"heex",
-				"eex",
-				"json",
-			},
-			-- Disable nvim-treesitter's highlighting, we use native
-			highlight = { enable = false },
-			indent = { enable = false },
-		})
-	end,
+	opts = {
+		ensure_installed = {
+			"bash",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"javascript",
+			"typescript",
+			"tsx",
+			"markdown",
+			"markdown_inline",
+			"vim",
+			"vimdoc",
+			"elixir",
+			"heex",
+			"eex",
+			"json",
+		},
+		highlight = { enable = true },
+	},
 }
