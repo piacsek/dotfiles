3e59c2b8230b1cdb56f3ea0d8a13a7d75e895a55
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-01-30 21:09:41 -0800

    [2026-01-30 21:09:41] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-01-30 21:09:41

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,24 +1,8 @@
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
-	keys = {
-		{
-			"<leader>4",
-			function()
-				keymap_deprecated_funsies("<leader>4")
-			end,
-		},
-		{
-			"<leader>8",
-			function()
-				keymap_deprecated_funsies("<leader>8")
-			end,
-		},
-		{
-			"<leader>9",
-			function()
-				keymap_deprecated_funsies("<leader>9")
-			end,
-		},
-	},
+	vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Go to the buffer on the left" }),
+	vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Go to the buffer on the bottom" }),
+	vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Go to the buffer on the top" }),
+	vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Go to the buffer on the right" }),
 }
