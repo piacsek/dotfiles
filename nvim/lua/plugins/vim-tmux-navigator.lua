00c9e0d695015773d02b1938dfe78c3566e615f2
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-01-30 21:08:33 -0800

    [2026-01-30 21:08:33] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-01-30 21:08:33

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,4 +1,24 @@
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
+	keys = {
+		{
+			"<leader>4",
+			function()
+				keymap_deprecated_funsies("<leader>4")
+			end,
+		},
+		{
+			"<leader>8",
+			function()
+				keymap_deprecated_funsies("<leader>8")
+			end,
+		},
+		{
+			"<leader>9",
+			function()
+				keymap_deprecated_funsies("<leader>9")
+			end,
+		},
+	},
 }
