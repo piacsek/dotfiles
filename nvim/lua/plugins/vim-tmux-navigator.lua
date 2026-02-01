7d5759d0e74da0a82c6089fcdbce61f54c5bcdde
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-01-31 19:40:35 -0800

    [2026-01-31 19:40:35] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-01-31 19:40:35

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -2,10 +2,16 @@ return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
 	keys = {
-		{ "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<M-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		-- { "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		-- { "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		-- { "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		-- { "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		-- { "<M-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 	},
+	config = function()
+		vim.keymap.set("n", "<M-h>", ":TmuxNavigateLeft<CR>")
+		vim.keymap.set("n", "<M-j>", ":TmuxNavigateDown<CR>")
+		vim.keymap.set("n", "<M-k>", ":TmuxNavigateUp<CR>")
+		vim.keymap.set("n", "<M-l>", ":TmuxNavigateRight<CR>")
+	end,
 }
