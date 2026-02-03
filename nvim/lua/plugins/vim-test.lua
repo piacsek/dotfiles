f4073ef72d7e829af9643e419ef7beccfc7540c7
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-03 11:55:18 -0800

    [2026-02-03 11:55:18] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-02-03 11:55:18

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -52,7 +52,7 @@ return {
 					actions = {
 						confirm = function(picker, item)
 							picker:close()
-							vim.notify(item)
+							vim.notify(table.concat(item, ", "))
 							-- Get all manually selected items (via Tab/Ctrl-a)
 							local sel = picker:get_selected()
 							local paths = {}
