022ca0cf0b605b4204c0ef10974e8e5f50144bb1
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-07 17:31:10 -0800

    [2026-02-07 17:31:10] Claude CLI not available, using default commit message Auto-sync dotfiles - 2026-02-07 17:31:10

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -12,19 +12,19 @@ local function setup_keymaps(harpoon)
 		harpoon:list():add()
 	end)
 
-	vim.keymap.set("n", "<M-j>", function()
+	vim.keymap.set("n", "<M-a>", function()
 		harpoon:list():select(1)
 	end)
 
-	vim.keymap.set("n", "<M-k>", function()
+	vim.keymap.set("n", "<M-s>", function()
 		harpoon:list():select(2)
 	end)
 
-	vim.keymap.set("n", "<M-l>", function()
+	vim.keymap.set("n", "<M-d>", function()
 		harpoon:list():select(4)
 	end)
 
-	vim.keymap.set("n", "<M-h>", function()
+	vim.keymap.set("n", "<M-f>", function()
 		harpoon:list():select(4)
 	end)
 end
