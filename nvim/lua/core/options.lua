b686eb720f83175a3e2705f1dc9511baec9ac572
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-09 12:06:59 -0800

    config: enable termguicolors option

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -2,6 +2,7 @@ vim.g.have_nerd_font = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.opt.termguicolors = true
 vim.opt.swapfile = false
 vim.opt.winborder = "rounded"
 vim.opt.autoread = true
