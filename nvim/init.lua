5c21292ee1f4f8aa7ad23dc0473e94acef21f56a
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-09 12:16:08 -0800

    config: switch from purple-rain to moonfly theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,9 +31,8 @@ require("lazy").setup("plugins", {
 -- if tonumber(os.date("%H")) <= 13 then
 -- vim.g._default_colorscheme = "cyberdream-light"
 -- else
--- vim.g._default_colorscheme = "moonfly"
+vim.g._default_colorscheme = "moonfly"
 -- vim.g._default_colorscheme = "high-contrast"
-vim.g._default_colorscheme = "purple-rain"
 -- end
 vim.cmd.colorscheme(vim.g._default_colorscheme)
 
