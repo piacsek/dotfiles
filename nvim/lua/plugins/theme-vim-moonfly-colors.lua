12f82038e0367af18785b532dc090605b3f076a1
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-09 18:52:00 -0800

    config: customize Elixir module colors to cyan

--- nvim/lua/plugins/theme-vim-moonfly-colors.lua
+++ nvim/lua/plugins/theme-vim-moonfly-colors.lua
@@ -3,4 +3,17 @@ return {
 	name = "moonfly",
 	lazy = false,
 	priority = 1000,
+	config = function()
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "moonfly",
+			callback = function()
+				-- Customize Elixir module colors to cyan
+				local cyan = "#80a0ff" -- moonfly's cyan color
+
+				vim.api.nvim_set_hl(0, "@module.elixir", { fg = cyan })
+				vim.api.nvim_set_hl(0, "@lsp.type.namespace.elixir", { fg = cyan })
+				vim.api.nvim_set_hl(0, "@type.elixir", { fg = cyan })
+			end,
+		})
+	end,
 }
