5f31e7f6a9fb74df9eea8b6753cc2c12a30b3800
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-09 18:54:34 -0800

    config: remove Elixir color customization code

--- nvim/lua/plugins/theme-vim-moonfly-colors.lua
+++ nvim/lua/plugins/theme-vim-moonfly-colors.lua
@@ -3,17 +3,4 @@ return {
 	name = "moonfly",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "moonfly",
-			callback = function()
-				-- Customize Elixir module colors to cyan
-				-- local cyan = "#37cccc" -- moonfly's cyan color
-				--
-				-- vim.api.nvim_set_hl(0, "@module.elixir", { fg = cyan })
-				-- vim.api.nvim_set_hl(0, "@lsp.type.namespace.elixir", { fg = cyan })
-				-- vim.api.nvim_set_hl(0, "@type.elixir", { fg = cyan })
-			end,
-		})
-	end,
 }
