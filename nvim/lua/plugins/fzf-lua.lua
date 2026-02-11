b3b8bdfc05a480397986376b0ba1016082d28052
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-10 20:52:30 -0800

    config: simplify script picker tmux command

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -81,17 +81,8 @@ local function run_script_picker(fzf)
 				if not selected or #selected == 0 then
 					return
 				end
-				local script_name = selected[1]
-				local full_path = nil
-				for _, entry in ipairs(entries) do
-					if entry.display == script_name then
-						full_path = entry.path
-						break
-					end
-				end
-				if full_path then
-				end
-				local cmd = string.format("tmux new-window '%s; read -p \"Press enter to close...\"'", full_path)
+				local cmd = string.format("tmux new-window '%s", selected[1])
+
 				vim.fn.system(cmd)
 			end,
 			["ctrl-s"] = function(selected)
