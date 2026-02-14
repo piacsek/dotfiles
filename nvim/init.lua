5a6709c4da994f0a05504a117c43153eb6e6f17d
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-02-14 13:27:49 -0800

    config: switch from high-contrast to oxide theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,7 +29,8 @@ require("lazy").setup("plugins", {
 })
 
 -- vim.g._default_colorscheme = "moonfly"
-vim.g._default_colorscheme = "high-contrast"
+-- vim.g._default_colorscheme = "high-contrast"
+vim.g._default_colorscheme = "oxide"
 vim.cmd.colorscheme(vim.g._default_colorscheme)
 
 vim.notify = require("snacks").notifier.notify
