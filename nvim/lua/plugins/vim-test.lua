310d924e64cb90976f2aba87bea2b36a94a6ff46
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-03-26 16:43:17 -0700

    nvim: remove duplicate debug notify

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -73,7 +73,6 @@ return {
 				for _, file in ipairs(output) do
 					local rel = file:gsub("^" .. vim.pesc(prefix), "")
 					vim.notify(rel)
-					vim.notify(vim.fn.fnameescape(rel))
 					table.insert(paths, vim.fn.fnameescape(rel))
 				end
 
