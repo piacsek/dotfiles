4352e89313ed7d96e4785e379b9eaddd7c04a1a7
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 18:20:24 -0700

    config: remove vim.pack Comment.nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,12 +34,6 @@ require("lazy").setup("plugins", {
 	},
 })
 
-local gh = function(x) return "https://github.com/" .. x end
-
-vim.pack.add({
-	gh("numToStr/Comment.nvim"),
-})
-
 -- vim.g._default_colorscheme = "moonfly"
 vim.g._default_colorscheme = "high-contrast"
 -- vim.g._default_colorscheme = "oxide"
