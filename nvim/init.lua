ded0e6440580046eaf42045647f6bccd082c5816
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 19:19:01 -0700

    config: use snacks picker, remove scratch keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,11 @@ vim.pack.add({
 	"https://github.com/folke/snacks.nvim",
 	"https://github.com/LintaoAmons/scratch.nvim",
 })
-require("snacks").setup({})
+require("snacks").setup({picker = {}})
 
 require("scratch").setup({
 	scratch_file_dir = "~/scratch.nvim",
 	window_cmd = "edit",
-	file_picker = "fzflua",
 	filetypes = { "ex", "lua", "js", "sh", "ts", "json", "heex", "html", "sql", "md" },
 })
 
-vim.keymap.set("n", "<leader>fs", ":ScratchOpen<CR>", { desc = "[J]ump to [S]cratch" })
-vim.keymap.set("n", "<leader>n", ":Scratch<CR>", { desc = "[N]ew scratch" })
