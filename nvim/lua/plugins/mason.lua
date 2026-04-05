51340e696f2a902b0dc44de8a8c9b8c39c8bcdb5
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 18:42:44 -0700

    config: remove mason.nvim plugin configuration

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,12 +1,1 @@
-return {
-    "mason-org/mason.nvim",
-    opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    }
-}
+return {}
