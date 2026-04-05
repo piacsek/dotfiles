2d548913bc87f1a2ef29d7929749fee97b3eb373
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 18:53:53 -0700

    config: add fzf keymaps and script picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -574,6 +574,117 @@ end
 fzf.setup(fzf_config)
 fzf.register_ui_select()
 
+local grep_winopts = {
+	height = 0.9,
+	width = 0.9,
+	preview = { hidden = "nohidden" },
+}
+
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg("v")
+	vim.fn.setreg("v", {})
+	text = string.gsub(text, "\n", "")
+	return #text > 0 and text or ""
+end
+
+local function run_script_picker()
+	local entries = vim.g._available_scripts
+	if entries == nil then
+		vim.notify("No available scripts configured. Please set vim.g._available_scripts", vim.log.levels.ERROR)
+		return
+	end
+
+	fzf.fzf_exec(function(fzf_cb)
+		for _, entry in ipairs(entries) do
+			fzf_cb(entry)
+		end
+		fzf_cb()
+	end, {
+		prompt = "Scripts> ",
+		winopts = { height = 0.4, width = 0.5 },
+		actions = {
+			["default"] = function(selected)
+				if selected and #selected == 1 then vim.fn.system(selected[1]) end
+			end,
+			["ctrl-w"] = function(selected)
+				if selected and #selected == 1 then vim.fn.system(string.format("tmux new-window '%s';", selected[1])) end
+			end,
+			["ctrl-s"] = function(selected)
+				if selected and #selected == 1 then vim.fn.system(string.format("tmux split-window -v -p 50 '%s';", selected[1])) end
+			end,
+			["ctrl-v"] = function(selected)
+				if selected and #selected == 1 then vim.fn.system(string.format("tmux split-window -h -p 50 '%s';", selected[1])) end
+			end,
+		},
+	})
+end
+
+-- Script runner
+vim.keymap.set("n", "<leader>r", run_script_picker, { desc = "[R]un script" })
+
+-- Find files
+vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "[F]ind [F]iles" })
+vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>ft", fzf.colorschemes, { desc = "[F]ind [T]heme" })
+vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "[F]ind [K]eymaps" })
+vim.keymap.set("n", "<leader>F", fzf.resume, { desc = "Resume last search" })
+vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { desc = "[F]ind [O] files" })
+
+vim.keymap.set("v", "<leader>ff", function()
+	local text = vim.getVisualSelection()
+	fzf.files({ query = text })
+end, { noremap = true, silent = true, desc = "[F]ind [F]files with selected text" })
+
+vim.keymap.set("n", "<leader>fp", function()
+	fzf.files({ cwd = vim.fn.getcwd() .. "/piacsek" })
+end, { desc = "[F]ind [P]iacsek files" })
+
+vim.keymap.set("n", "<leader>fd", function()
+	fzf.files({ cwd = "~/dotfiles" })
+end, { desc = "[F]ind [D]otfiles" })
+
+vim.keymap.set("v", "<leader>fm", function()
+	local text = vim.getVisualSelection()
+	fzf.git_status({ query = text })
+end, { noremap = true, silent = true, desc = "[F]ind [M]odified git files with selection" })
+
+-- Search
+vim.keymap.set("v", "<leader>ss", function()
+	local text = vim.getVisualSelection()
+	fzf.live_grep({ search = text, winopts = grep_winopts })
+end, { desc = "[G]rep selected" })
+
+vim.keymap.set("n", "<leader>ss", function()
+	fzf.live_grep({ winopts = grep_winopts })
+end, { desc = "[F]ind by [G]rep" })
+
+vim.keymap.set("n", "<leader>sc", function()
+	fzf.live_grep({ cwd = "~/dotfiles", winopts = grep_winopts })
+end, { desc = "Grep config files" })
+
+vim.keymap.set("v", "<leader>sc", function()
+	local text = vim.getVisualSelection()
+	fzf.live_grep({ cwd = "~/dotfiles", search = text, winopts = grep_winopts })
+end, { desc = "[G]rep selected" })
+
+vim.keymap.set("n", "<leader>/", function() fzf.blines() end, { desc = "[/] Fuzzily search in current buffer" })
+
+vim.keymap.set("v", "<leader>/", function()
+	local text = vim.getVisualSelection()
+	fzf.blines({ query = text })
+end, { desc = "[/] Fuzzily search in current buffer" })
+
+-- Git
+vim.keymap.set("n", "<leader>gh", fzf.git_bcommits, { desc = "[G]it [H]istory" })
+vim.keymap.set("n", "<leader>fm", fzf.git_status, { desc = "[F]ind [M]odified git files" })
+vim.keymap.set("n", "<leader>gsm", function()
+	fzf.git_commits({
+		cmd = "git log --color --pretty=format:'%C(yellow)%h%Creset %Cgreen(%><(12)%cr%><|(12))%Creset %s %C(blue)<%an>%Creset' main",
+	})
+end, { desc = "[G]it [S]earch [M]ain branch commits" })
+
 -- Colorscheme
 -- vim.g._default_colorscheme = "moonfly"
 vim.g._default_colorscheme = "high-contrast"
