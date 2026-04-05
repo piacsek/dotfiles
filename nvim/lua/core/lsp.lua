88ad466d511a29a98232063c1defb0c58dc36670
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-05 12:32:14 -0700

    config: defer tailwindcss root_dir callback

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -24,9 +24,9 @@ vim.lsp.config("tailwindcss", {
 		if project_tailwind_root then
 			local path = vim.fn.expand(project_tailwind_root)
 			path = vim.uv.fs_realpath(path) or path
-			on_dir(path)
-		else
-			vim.notify("tailwindls unavailable: Please define tailwind_root.")
+			vim.schedule(function()
+				on_dir(path)
+			end)
 		end
 	end,
 })
