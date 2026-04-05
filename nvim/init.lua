f10c579b1bcd2ae865cb6335cf800b5c55ad1e9a
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 18:51:37 -0700

    config: migrate plugins from lazy.nvim to vim.pack

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,7 @@ local gh = function(x)
 end
 
 vim.pack.add({
+	-- Simple plugins
 	gh("numToStr/Comment.nvim"),
 	gh("windwp/nvim-autopairs"),
 	gh("echasnovski/mini.ai"),
@@ -20,6 +21,8 @@ vim.pack.add({
 	gh("sotte/presenting.nvim"),
 	gh("herisetiawan00/jtt.nvim"),
 	gh("christoomey/vim-tmux-navigator"),
+
+	-- Colorschemes
 	gh("scottmckendry/cyberdream.nvim"),
 	gh("piacsek/high-contrast.nvim"),
 	gh("AlexvZyl/nordic.nvim"),
@@ -28,12 +31,44 @@ vim.pack.add({
 	gh("oxidescheme/oxide.nvim"),
 	gh("dmkc/underwater-vim-theme"),
 	gh("bluz71/vim-moonfly-colors"),
+
+	-- LSP & tooling
 	gh("mason-org/mason.nvim"),
 	gh("neovim/nvim-lspconfig"),
 	gh("folke/lazydev.nvim"),
 	gh("lewis6991/gitsigns.nvim"),
+	gh("stevearc/conform.nvim"),
+	gh("nvim-treesitter/nvim-treesitter"),
+
+	-- File navigation
+	gh("stevearc/oil.nvim"),
+	{ src = gh("ThePrimeagen/harpoon"), version = "harpoon2" },
+	gh("nvim-lua/plenary.nvim"),
+	gh("ibhagwan/fzf-lua"),
+
+	-- Search & replace
+	gh("nvim-pack/nvim-spectre"),
+	gh("LintaoAmons/scratch.nvim"),
+
+	-- Completion
+	gh("hrsh7th/nvim-cmp"),
+	gh("L3MON4D3/LuaSnip"),
+	gh("saadparwaiz1/cmp_luasnip"),
+	gh("hrsh7th/cmp-nvim-lsp"),
+	gh("hrsh7th/cmp-path"),
+
+	-- Testing
+	gh("vim-test/vim-test"),
+	gh("preservim/vimux"),
+
+	-- UI
+	gh("folke/snacks.nvim"),
 }, { load = true })
 
+-- Dev plugins
+vim.opt.rtp:prepend(vim.fn.expand("~/projects/nvim-plugins/buddy.nvim"))
+
+-- Plugin setup
 require("nvim-autopairs").setup({})
 require("mini.ai").setup({})
 require("mini.icons").setup({})
@@ -83,32 +118,331 @@ require("mason").setup({
 		},
 	},
 })
+require("conform").setup({
+	notify_on_error = false,
+	format_on_save = function(bufnr)
+		local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true }
+		return {
+			timeout_ms = 3000,
+			lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		}
+	end,
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		css = { "prettierd", "prettier", stop_after_first = true },
+		html = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
+
+-- Treesitter
+vim.api.nvim_create_user_command("TSInstallParsers", function()
+	require("nvim-treesitter").install({
+		"bash",
+		"diff",
+		"html",
+		"lua",
+		"luadoc",
+		"javascript",
+		"typescript",
+		"tsx",
+		"markdown",
+		"markdown_inline",
+		"vim",
+		"vimdoc",
+		"elixir",
+		"heex",
+		"eex",
+		"json",
+	})
+end, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"bash",
+		"html",
+		"lua",
+		"javascript",
+		"typescript",
+		"markdown",
+		"vim",
+		"elixir",
+		"heex",
+		"eex",
+		"json",
+	},
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+
+-- Oil
+require("oil").setup({
+	view_options = { show_hidden = true },
+	keymaps = {
+		["<CR>"] = { "actions.select" },
+		["-"] = { "actions.parent", mode = "n" },
+		["_"] = { "actions.open_cwd", mode = "n" },
+		["<leader>tt"] = {
+			callback = function()
+				local oil = require("oil")
+				local entry = oil.get_cursor_entry()
+
+				if not entry then
+					vim.notify("No file under cursor", vim.log.levels.WARN)
+					return
+				end
+
+				local dir = oil.get_current_dir()
+				local filepath = dir .. entry.name
+
+				vim.cmd("TestSuite " .. filepath)
+			end,
+			desc = "Run test file under cursor",
+		},
+		["<leader>x"] = {
+			callback = function()
+				local oil = require("oil")
+				local entry = oil.get_cursor_entry()
+
+				if not entry then
+					vim.notify("No file under cursor", vim.log.levels.WARN)
+					return
+				end
+
+				local filepath = oil.get_current_dir() .. entry.name
+
+				vim.fn.system("chmod +x " .. vim.fn.shellescape(filepath))
+				vim.notify("Made executable: " .. entry.name, vim.log.levels.INFO)
+			end,
+			desc = "Make file executable (chmod +x)",
+		},
+	},
+	use_default_keymaps = false,
+})
+
+-- Harpoon
+local harpoon = require("harpoon")
+harpoon:setup()
+
+-- Spectre
+require("spectre").setup({
+	open_cmd = function()
+		vim.cmd("noautocmd new")
+		vim.api.nvim_win_set_config(0, {
+			relative = "editor",
+			width = math.floor(vim.o.columns * 0.8),
+			height = math.floor(vim.o.lines * 0.8),
+			row = math.floor(vim.o.lines * 0.1),
+			col = math.floor(vim.o.columns * 0.1),
+			style = "minimal",
+			border = "rounded",
+		})
+	end,
+})
+
+-- Scratch
+require("scratch").setup({
+	scratch_file_dir = "~/scratch.nvim",
+	window_cmd = "edit",
+	file_picker = "fzflua",
+	filetypes = { "ex", "lua", "js", "sh", "ts", "json", "heex", "html", "sql", "md" },
+})
+
+-- nvim-cmp
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+luasnip.config.setup({})
+require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets" })
+
+cmp.setup({
+	view = {
+		docs = {
+			auto_open = false,
+		},
+	},
+	window = {
+		completion = {
+			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+			col_offset = -3,
+			side_padding = 0,
+			max_width = 60,
+			max_height = 15,
+		},
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-k>"] = cmp.mapping(function()
+			if cmp.visible_docs() then
+				cmp.close_docs()
+			else
+				cmp.open_docs()
+			end
+		end),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "path" },
+	},
+})
+
+-- vim-test
+vim.g["test#strategy"] = "vimux"
+vim.g["test#preserve_screen"] = 0
+vim.g["test#echo_command"] = 0
+vim.g["test#neovim#term_position"] = "topleft vsplit"
+vim.g["test#neovim_sticky#kill_previous"] = 1
+
+-- Snacks
+require("snacks").setup({
+	notifier = {},
+	picker = {
+		sources = {
+			explorer = {
+				actions = {
+					run_test_file = function(picker)
+						local item = picker:current()
+						if not item or not item.file then
+							vim.notify("Nothing selected", vim.log.levels.WARN)
+							return
+						end
+
+						vim.cmd("TestSuite " .. vim.fn.fnameescape(item.file))
+					end,
+					search_in_dir = function(picker)
+						local item = picker:current()
+						if not item then
+							vim.notify("Nothing selected", vim.log.levels.WARN)
+							return
+						end
+
+						local dir_path
+						if item.file then
+							if vim.fn.isdirectory(item.file) == 1 then
+								dir_path = item.file
+							else
+								dir_path = vim.fn.fnamemodify(item.file, ":h")
+							end
+						else
+							dir_path = vim.fn.getcwd()
+						end
+
+						picker:close()
+
+						require("fzf-lua").live_grep({
+							cwd = dir_path,
+							winopts = {
+								height = 0.9,
+								width = 0.9,
+								preview = {
+									hidden = "nohidden",
+								},
+							},
+						})
+					end,
+				},
+				win = {
+					list = {
+						keys = {
+							["<leader>tt"] = "run_test_file",
+							["<leader>ss"] = "search_in_dir",
+						},
+					},
+				},
+			},
+		},
+	},
+	bigfile = { enabled = false },
+	bufdelete = { enabled = false },
+	dashboard = { enabled = false },
+	debug = { enabled = false },
+	dim = { enabled = false },
+	git = { enabled = false },
+	indent = { enabled = false },
+	profiler = { enabled = false },
+	quickfile = { enabled = false },
+	rename = { enabled = false },
+	scope = { enabled = false },
+	scroll = { enabled = false },
+	statuscolumn = { enabled = false },
+	toggle = { enabled = false },
+	words = { enabled = false },
+	zen = { enabled = false },
+})
+
+-- fzf-lua
+local fzf = require("fzf-lua")
+
+local function open_commit_on_browser(selected)
+	if not selected or #selected == 0 then
+		return
+	end
+	local commit = selected[1]:match("^(%S+)")
+	if commit then
+		require("snacks").gitbrowse({ commit = commit })
+	end
+end
+
+local fzf_config = {
+	winopts = {
+		height = 0.4,
+		width = 0.5,
+		row = 0.5,
+		col = 0.5,
+		preview = {
+			hidden = "hidden",
+		},
+	},
+	keymap = {
+		fzf = {
+			true,
+			["ctrl-q"] = "select-all+accept",
+		},
+	},
+	git = {
+		bcommits = { actions = { ["ctrl-w"] = open_commit_on_browser } },
+		commits = { actions = { ["ctrl-w"] = open_commit_on_browser } },
+	},
+}
+
+local cwd = vim.fn.getcwd()
+local local_config_path = cwd .. "/piacsek/fzf.lua"
+
+if vim.fn.filereadable(local_config_path) == 1 then
+	local ok, local_config = pcall(dofile, local_config_path)
+	if ok and local_config then
+		fzf_config = vim.tbl_deep_extend("force", fzf_config, local_config)
+	end
+end
+
+fzf.setup(fzf_config)
+fzf.register_ui_select()
+
+-- Colorscheme
 -- vim.g._default_colorscheme = "moonfly"
 vim.g._default_colorscheme = "high-contrast"
 -- vim.g._default_colorscheme = "oxide"
 vim.cmd.colorscheme(vim.g._default_colorscheme)
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-	dev = {
-		path = "~/projects/nvim-plugins",
-	},
-})
 
 vim.notify = require("snacks").notifier.notify
 
