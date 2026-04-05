69e495b30d80ead7e28f13ed494c8e4090d84f5b
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 19:20:18 -0700

    config: format spacing and remove trailing newline

--- nvim/scratch-minimal-init.lua
+++ nvim/scratch-minimal-init.lua
@@ -2,11 +2,10 @@ vim.pack.add({
 	"https://github.com/folke/snacks.nvim",
 	"https://github.com/LintaoAmons/scratch.nvim",
 })
-require("snacks").setup({picker = {}})
+require("snacks").setup({ picker = {} })
 
 require("scratch").setup({
 	scratch_file_dir = "~/scratch.nvim",
 	window_cmd = "edit",
 	filetypes = { "ex", "lua", "js", "sh", "ts", "json", "heex", "html", "sql", "md" },
 })
-
