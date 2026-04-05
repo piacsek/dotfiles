71b1595801046a90b3b1a0472ad7343f855c34d5
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-04 18:30:44 -0700

    config: disable jtt.nvim plugin

--- nvim/lua/plugins/jtt.lua
+++ nvim/lua/plugins/jtt.lua
@@ -1,9 +1,1 @@
-return {
-	"herisetiawan00/jtt.nvim",
-	config = function()
-		require("jtt").setup()
-	end,
-	keys = {
-		{ "<leader>jt", "<cmd>JumpTest<CR>", desc = "[J]ump to [T]est" },
-	},
-}
+return {}
