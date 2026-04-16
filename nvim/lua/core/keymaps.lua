fb54b8086d3c741419fd9fbf57f1008c67d0af00
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-16 13:50:26 -0700

    config: make diagnostic float focusable

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -90,7 +90,9 @@ vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Pastes content without losi
 
 vim.keymap.set("n", "[", vim.diagnostic.get_next, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]", vim.diagnostic.get_prev, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>d", function ()
+	vim.diagnostic.open_float({focusable: true})
+end, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set({ "n", "v" }, "<C-s>", ":w<CR>", { desc = "[S]ave" })
 
