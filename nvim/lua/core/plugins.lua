d0d490819ce6808a537ece1533628e7414be8c4c
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-17 10:52:29 -0700

    config: setup markdown-preview plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -47,6 +47,13 @@ vim.pack.add({
 
 -- Dev plugins
 -- vim.opt.rtp:prepend(vim.fn.expand("~/projects/nvim-plugins/buddy.nvim")) Plugin setup
+require("markdown_preview").setup({
+	-- all optional; sane defaults shown
+	instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+	port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
+	open_browser = true,
+	debounce_ms = 300,
+})
 -- require("claudecode").setup({ terminal = { provider = "none" } })
 require("nvim-autopairs").setup({})
 require("mini.ai").setup({})
