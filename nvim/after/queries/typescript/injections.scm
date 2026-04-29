88840f8cbba4d1f77d3beab005d08b45e6a04ece
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-28 17:44:22 -0700

    config: detect sql in ts template strings by content

--- nvim/after/queries/typescript/injections.scm
+++ nvim/after/queries/typescript/injections.scm
@@ -1,8 +1,26 @@
 ; extends
 
-((call_expression
-   function: (identifier) @_name
-   arguments: (template_string) @injection.content)
- (#eq? @_name "sql")
+((template_string) @injection.content
+ (#lua-match? @injection.content "^`[%s\n]*%-?%-?[%s\n]*[Ss][Ee][Ll][Ee][Cc][Tt][%s\n]")
+ (#set! injection.language "sql")
+ (#offset! @injection.content 0 1 0 -1))
+
+((template_string) @injection.content
+ (#lua-match? @injection.content "^`[%s\n]*[Ww][Ii][Tt][Hh][%s\n]")
+ (#set! injection.language "sql")
+ (#offset! @injection.content 0 1 0 -1))
+
+((template_string) @injection.content
+ (#lua-match? @injection.content "^`[%s\n]*[Ii][Nn][Ss][Ee][Rr][Tt][%s\n]")
+ (#set! injection.language "sql")
+ (#offset! @injection.content 0 1 0 -1))
+
+((template_string) @injection.content
+ (#lua-match? @injection.content "^`[%s\n]*[Uu][Pp][Dd][Aa][Tt][Ee][%s\n]")
+ (#set! injection.language "sql")
+ (#offset! @injection.content 0 1 0 -1))
+
+((template_string) @injection.content
+ (#lua-match? @injection.content "^`[%s\n]*[Dd][Ee][Ll][Ee][Tt][Ee][%s\n]")
  (#set! injection.language "sql")
  (#offset! @injection.content 0 1 0 -1))
