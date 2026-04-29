6948a8e5d292a4ccf6fec091906202e240e40f43
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-28 17:49:24 -0700

    config: re-add SELECT match for ts sql injection

--- nvim/after/queries/typescript/injections.scm
+++ nvim/after/queries/typescript/injections.scm
@@ -1,5 +1,7 @@
 ; extends
 
 ((template_string) @injection.content
+ (#lua-match? @injection.content "SELECT")
  (#set! injection.language "sql")
+ (#set! injection.include-children)
  (#offset! @injection.content 0 1 0 -1))
