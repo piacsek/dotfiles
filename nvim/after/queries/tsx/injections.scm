782946e25c897f46e91cbe7d24dccd11e0d33aa8
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-28 17:49:30 -0700

    config: include children in tsx sql injection

--- nvim/after/queries/tsx/injections.scm
+++ nvim/after/queries/tsx/injections.scm
@@ -3,4 +3,5 @@
 ((template_string) @injection.content
  (#lua-match? @injection.content "SELECT")
  (#set! injection.language "sql")
+ (#set! injection.include-children)
  (#offset! @injection.content 0 1 0 -1))
