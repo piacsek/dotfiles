ab60416a4f01279635c1ab25490e2eccb519d07e
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-04-28 17:46:37 -0700

    config: simplify tsx sql injection to match SELECT

--- nvim/after/queries/tsx/injections.scm
+++ nvim/after/queries/tsx/injections.scm
@@ -1,26 +1,6 @@
 ; extends
 
 ((template_string) @injection.content
- (#lua-match? @injection.content "^`[%s\n]*%-?%-?[%s\n]*[Ss][Ee][Ll][Ee][Cc][Tt][%s\n]")
- (#set! injection.language "sql")
- (#offset! @injection.content 0 1 0 -1))
-
-((template_string) @injection.content
- (#lua-match? @injection.content "^`[%s\n]*[Ww][Ii][Tt][Hh][%s\n]")
- (#set! injection.language "sql")
- (#offset! @injection.content 0 1 0 -1))
-
-((template_string) @injection.content
- (#lua-match? @injection.content "^`[%s\n]*[Ii][Nn][Ss][Ee][Rr][Tt][%s\n]")
- (#set! injection.language "sql")
- (#offset! @injection.content 0 1 0 -1))
-
-((template_string) @injection.content
- (#lua-match? @injection.content "^`[%s\n]*[Uu][Pp][Dd][Aa][Tt][Ee][%s\n]")
- (#set! injection.language "sql")
- (#offset! @injection.content 0 1 0 -1))
-
-((template_string) @injection.content
- (#lua-match? @injection.content "^`[%s\n]*[Dd][Ee][Ll][Ee][Tt][Ee][%s\n]")
+ (#lua-match? @injection.content "SELECT")
  (#set! injection.language "sql")
  (#offset! @injection.content 0 1 0 -1))
