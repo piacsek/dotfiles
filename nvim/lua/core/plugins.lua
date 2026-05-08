fff57a3f4d50e704f90dba389f3a811d32017cb1
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-05-08 13:56:17 -0700

    nvim: update conform lsp_format option

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -134,10 +134,10 @@ require("mason").setup({
 require("conform").setup({
 	notify_on_error = false,
 	format_on_save = function(bufnr)
-		local disable_filetypes = { c = true, cpp = true, yaml = true, yml = true }
+		local disable_filetypes = { c = true, cpp = true, yaml = true }
 		return {
 			timeout_ms = 3000,
-			lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+			lsp_format = disable_filetypes[vim.bo[bufnr].filetype] and "never" or "fallback",
 		}
 	end,
 	formatters_by_ft = {
