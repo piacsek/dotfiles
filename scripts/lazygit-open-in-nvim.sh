a7dde6dc5c926f3938a351391440e7b877b05472
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-05-22 11:17:53 -0700

    lazygit: focus nvim pane after opening file

--- scripts/lazygit-open-in-nvim.sh
+++ scripts/lazygit-open-in-nvim.sh
@@ -9,3 +9,7 @@ pane=$(tmux list-panes -t :1 -F '#{pane_id} #{pane_current_command}' \
   | awk '/nvim/{print $1; exit}')
 
 tmux send-keys -t "${pane:-:1}" Escape ":edit $abs" Enter
+tmux select-window -t :1
+if [[ -n "$pane" ]]; then
+  tmux select-pane -t "$pane"
+fi
