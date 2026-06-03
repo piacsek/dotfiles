ec5ca4982ef417329e69ac4250061251c85657d3
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-06-02 18:27:26 -0700

    config: let ghostty-mirror manage background

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -79,7 +79,7 @@ hipatterns.setup({
 })
 require("nvim-ts-autotag").setup({})
 -- Mirror the colorscheme into Ghostty and tmux on :colorscheme.
-require("ghostty-mirror").setup({ tmux = { enabled = true } })
+require("ghostty-mirror").setup({ manage_background = true, tmux = { enabled = true } })
 require("url-open").setup({
 	highlight_url = {
 		cursor_move = {
